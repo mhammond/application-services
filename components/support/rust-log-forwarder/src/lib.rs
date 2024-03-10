e569b4e3b39f0056fca92e008db480791fd5d372
Mark Hammond <mhammond@skippinet.com.au>
2024-03-09 22:58:39 -0500

    wire up log forwarder

--- components/support/rust-log-forwarder/src/lib.rs
+++ components/support/rust-log-forwarder/src/lib.rs
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 mod foreign_logger;
 mod rust_logger;
@@ -9,6 +10,9 @@ mod rust_logger;
 pub use foreign_logger::{AppServicesLogger, Level, Record};
 
 static HAVE_SET_MAX_LEVEL: AtomicBool = AtomicBool::new(false);
+
+// The "targets" (in practice, crate names) which are hooked up to the `tracing` crate for logging.
+static TRACING_TARGETS: &'static [&'static str] = &["autofill"];
 
 /// Set the logger to forward to.
 ///
@@ -18,7 +22,32 @@ pub fn set_logger(logger: Option<Box<dyn AppServicesLogger>>) {
     if !HAVE_SET_MAX_LEVEL.load(Ordering::Relaxed) {
         set_max_level(Level::Debug);
     }
+
+    // Set up a tracing subscriber for crates which use tracing and forward to the log forwarder.
+    let sink = Arc::new(ForwarderEventSink {});
+    for target in TRACING_TARGETS {
+        tracing_support::register_event_sink(target, tracing_support::Level::Trace, sink.clone())
+    }
     rust_logger::set_foreign_logger(logger)
+}
+
+struct ForwarderEventSink;
+
+impl tracing_support::EventSink for ForwarderEventSink {
+    fn on_event(&self, event: tracing_support::Event) {
+        let record = Record {
+            level: match event.level {
+                tracing_support::Level::Trace => Level::Trace,
+                tracing_support::Level::Debug => Level::Debug,
+                tracing_support::Level::Info => Level::Info,
+                tracing_support::Level::Warn => Level::Warn,
+                tracing_support::Level::Error => Level::Error,
+            },
+            target: event.target,
+            message: event.message,
+        };
+        rust_logger::forward_to_foreign_logger(record);
+    }
 }
 
 /// Set the maximum log level filter.  Records below this level will not be sent to the logger.
